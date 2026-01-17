40377a653e9ef67598c3a19887a48664c73e69cc
Ariane Paradis <115097778+A-Paradis@users.noreply.github.com>
2026-01-16 19:20:11 -0500

    chore: Added editor and object enum and interfaces

---
+++ common/enum.ts
@@ -0,0 +1,29 @@
+export enum GameMode {
+    CLASSIC = 'CLASSIC',
+    CTF = 'CTF'
+}
+
+export enum MapSize {
+    S = '10x10',
+    M = '15x15',
+    L = '20x20'
+}
+
+export enum TileType {
+    DIRT = 'DIRT',
+    WATER = 'WATER',
+    ICE = 'ICE',
+    WALL = 'WALL'
+}
+
+export enum ObjectType {
+    START = 'START',
+    FLAG = 'FLAG',
+    REGEN = 'REGEN',
+    ARENA = 'ARENA'
+}
+
+export enum ObjectSize {
+    S = '1x1',
+    L = '2x2'
+}
