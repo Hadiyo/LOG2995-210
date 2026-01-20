3e0823b1722433ca6d75a41950a4881d2038af39
Nadim Najjar <nadim.najjar@live.com>
2026-01-20 14:04:48 -0500

    fix(component): Fixed the buttons and made them more abstract to re-use

--- client/src/app/pages/main-page/main-page.component.ts
+++ client/src/app/pages/main-page/main-page.component.ts
@@ -1,16 +1,42 @@
 import { Component } from '@angular/core';
-import { MainActionsComponent } from '@app/components/main-actions/main-actions.component';
+import { AppButtonComponent } from '@app/components/app-button/app-button.component';
 import { NameSliderComponent } from '@app/components/name-slider/name-slider.component';
 
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
   styleUrls: ['./main-page.component.scss'],
-  imports: [MainActionsComponent, NameSliderComponent],
+  imports: [AppButtonComponent, NameSliderComponent],
   standalone: true,
 })
 export class MainPageComponent {
   readonly title: string = 'LOG2995';
+  readonly actions: readonly {
+    id: 'create-game' | 'join-game' | 'admin-game';
+    label: string;
+    variant: 'primary' | 'secondary' | 'ghost';
+    link?: string;
+    disabled?: boolean;
+  }[] = [
+    {
+      id: 'create-game',
+      label: 'CREER UNE PARTIE',
+      link: '/game',
+      variant: 'primary',
+    },
+    {
+      id: 'join-game',
+      label: 'JOINDRE UNE PARTIE',
+      link: '/material',
+      variant: 'secondary',
+    },
+    {
+      id: 'admin-game',
+      label: 'ADMINISTRER LES JEUX',
+      variant: 'ghost',
+      disabled: true,
+    },
+  ];
 
   readonly teamNames = [
     'Nadim',
