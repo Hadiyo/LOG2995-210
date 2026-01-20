f2f58f4c38ab3310fedb1b47c27fa5acb254de42
Thng04 <tomnguyen6688@gmail.com>
2026-01-19 22:47:12 -0500

    feat: added descriptions to toolbar

--- client/src/main.ts
+++ client/src/main.ts
@@ -8,7 +8,6 @@ import { GamePageComponent } from '@app/pages/game-page/game-page.component';
 import { MainPageComponent } from '@app/pages/main-page/main-page.component';
 import { MaterialPageComponent } from '@app/pages/material-page/material-page.component';
 import { environment } from './environments/environment';
-import { EditorPageComponent } from './app/pages/editor-page/editor-page.component';
 
 
 if (environment.production) {
