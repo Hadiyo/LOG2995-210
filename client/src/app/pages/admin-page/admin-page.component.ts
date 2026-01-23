342ca6996679da7005161a47ca12c8804720ad19
Nadim Najjar <nadim.najjar@live.com>
2026-01-22 22:04:50 -0500

    feat(admin): add admin page with create game entry

--- client/src/app/pages/admin-page/admin-page.component.ts
+++ client/src/app/pages/admin-page/admin-page.component.ts
@@ -1,9 +1,26 @@
 import { Component } from '@angular/core';
+import { AppButtonComponent } from '@app/components/app-button/app-button.component';
+import { CreateGameDialogComponent } from '@app/components/create-game-dialog/create-game-dialog.component';
 
 @Component({
   selector: 'app-admin-page',
   standalone: true,
+  imports: [AppButtonComponent, CreateGameDialogComponent],
   templateUrl: './admin-page.component.html',
   styleUrl: './admin-page.component.scss',
 })
-export class AdminPageComponent {}
+export class AdminPageComponent {
+  isCreateDialogOpen = false;
+
+  openCreateGameDialog(): void {
+    this.isCreateDialogOpen = true;
+  }
+
+  closeCreateGameDialog(): void {
+    this.isCreateDialogOpen = false;
+  }
+
+  handleCreateGameDialog(): void {
+    this.isCreateDialogOpen = false;
+  }
+}
