5bc61bdcfcfbbbad242a0b53546db99b778dbba6
Ariane Paradis <115097778+A-Paradis@users.noreply.github.com>
2026-02-11 10:27:05 -0500

    test: fixed test error issue

--- server/app/gateways/map/map.gateway.spec.ts
+++ server/app/gateways/map/map.gateway.spec.ts
@@ -1,12 +1,22 @@
+import { MapGateway } from '@app/gateways/map/map.gateway';
+import { MapService } from '@app/services/map/map.service';
 import { Test, TestingModule } from '@nestjs/testing';
-import { MapGateway } from './map.gateway';
 
 describe('MapGateway', () => {
   let gateway: MapGateway;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [MapGateway],
+      providers: [
+        MapGateway,
+        {
+          provide: MapService,
+          useValue: {
+            on: jest.fn(),
+            off: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     gateway = module.get<MapGateway>(MapGateway);
