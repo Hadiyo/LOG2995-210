5c689e3e8596e08e2425021c4ee4d6264951aaf0
Thng04 <tomnguyen6688@gmail.com>
2026-03-08 17:33:14 -0400

    chore(pull): pull feat/join-game

--- common/player/player.interface.ts
+++ common/player/player.interface.ts
@@ -1,12 +1,13 @@
 import { AvatarId, Die } from '../character/character.model';
 import { Vec2 } from '../maps/map.interface';
 
-// // Represents the status of a player in the game session
-// export enum PlayerStatus {
-//     Active = 'ACTIVE',
-//     Surrendered = 'SURRENDERED', // When the player quit voluntarily
-//     Eliminated = 'ELIMINATED', // When the player is defeated
-// }
+
+// Represents the status of a player in the game session WEI'S
+export enum PlayerStatus {
+    Active = 'ACTIVE',
+    Surrendered = 'SURRENDERED', // When the player quit voluntarily
+    Eliminated = 'ELIMINATED', // When the player is defeated
+}
 
 // Facing orientation used by the renderer.
 export type PlayerFacing = 'front' | 'right' | 'back' | 'left';
@@ -46,6 +47,7 @@ export interface PlayerRenderState {
     poseDurationMs?: number;
 }
 
+// Player to player info to test changes
 export interface Player {
     id: string;
     information: PlayerInformation;
