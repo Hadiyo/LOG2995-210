ba9e67067000a93e534fab552bdbb9a670598c43
fallou <fallouag@gmail.com>
2026-03-11 20:58:30 -0400

    waiting room test fix

--- server/app/gateways/waiting-room/waiting-room.gateway.spec.ts
+++ server/app/gateways/waiting-room/waiting-room.gateway.spec.ts
@@ -1,12 +1,31 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { WaitingRoomGateway } from './waiting-room.gateway';
+import { GameSessionService } from '@app/services/session/game-session.service';
+import { PlayerService } from '@app/services/player/player.service';
 
 describe('WaitingRoomGateway', () => {
   let gateway: WaitingRoomGateway;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [WaitingRoomGateway],
+      providers: [
+        WaitingRoomGateway,
+        {
+          provide: GameSessionService,
+          useValue: {
+            leaveGameSession: jest.fn(),
+            deleteGameSession: jest.fn(),
+            getGameSessionById: jest.fn(),
+          },
+        },
+        {
+          provide: PlayerService,
+          useValue: {
+            getPlayerBySocketId: jest.fn(),
+            getPlayerByName: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     gateway = module.get<WaitingRoomGateway>(WaitingRoomGateway);
